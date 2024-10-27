0c6cda9bf3331799e0afea280f71267cae1832f9
tubone <tubo.yyyuuu@gmail.com>
2024-10-27 16:11:29 +0900

    タイポ修正

--- bastion.tf
+++ bastion.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "postgre_backup_vm" {
-  name                = "postgre-backup-vm-pubic-ip"
+  name                = "postgre-backup-vm-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
